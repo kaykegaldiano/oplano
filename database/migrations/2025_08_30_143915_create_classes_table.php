e19c11e872d35397f27e55a57702ec4e7832a681
Kayke Galdiano <kaykegaldiano@gmail.com>
2025-08-30 23:17:31 -0300

    refactor: rename table from "courses" to "classes" in migration

--- database/migrations/2025_08_30_143915_create_classes_table.php
+++ database/migrations/2025_08_30_143915_create_classes_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('courses', function (Blueprint $table) {
+        Schema::create('classes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('code')->unique();
@@ -33,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('courses');
+        Schema::dropIfExists('classes');
     }
 };
