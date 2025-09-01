e2f10ca760d68bcf3e6a608c704d436b687a8deb
Kayke Galdiano <kaykegaldiano@gmail.com>
2025-08-31 23:23:29 -0300

    feat: register new Filament providers
    - Add `CsPanelProvider` to providers list
    - Add `MonitorPanelProvider` to providers list

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\CsPanelProvider::class,
     App\Providers\Filament\FilamentHookServiceProvider::class,
+    App\Providers\Filament\MonitorPanelProvider::class,
 ];
